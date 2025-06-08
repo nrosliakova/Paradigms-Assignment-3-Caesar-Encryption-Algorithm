4ff81312b8223ed09b192282ff0a8aaf94e1d4f2
Nina Rosliakova <nrosliakova@kse.org.ua>
2025-06-08 14:37:31 +0300

    func create map

--- CaesarCipher/CaesarCipher.cpp
+++ CaesarCipher/CaesarCipher.cpp
@@ -1,2 +1,18 @@
 
 #include "CaesarCipher.h"
+#include <stdio.h>
+#include <map>
+using namespace std;
+
+map<char, char> create_map(int key) {
+	map<char, char> dict;
+	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+	while (key > 26) key -= 26;
+	while (key < -26) key += 26;
+	for (int i = 0; i < strlen(alphabet); i++) {
+		int key_ = (i + key > 26) ? i + key - 26 : i + key;
+		//dict.insert({ alphabet[i], alphabet[key_] });
+		dict[alphabet[i]] = alphabet[key_];
+	}
+	return dict;
+}
