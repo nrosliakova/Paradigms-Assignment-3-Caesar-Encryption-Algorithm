1022aee48961a1be94c5b8c9e1e9ec80f5c09543
Nina Rosliakova <nrosliakova@kse.org.ua>
2025-06-08 14:57:56 +0300

    update header CeasarCipher

--- CaesarCipher/CaesarCipher.h
+++ CaesarCipher/CaesarCipher.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#ifdef CAESAR_CIPHER_EXPORTS
+#define CAESAR_CIPHER_API __declspec(dllexport)
+#else
+#define CAESAR_CIPHER_API __declspec(dllimport)
+#endif
+
+
+extern "C"
+{
+	CAESAR_CIPHER_API char* encrypt(char* rawText, int key);
+
+	CAESAR_CIPHER_API char* decrypt(char* encryptedText, int key);
+};
